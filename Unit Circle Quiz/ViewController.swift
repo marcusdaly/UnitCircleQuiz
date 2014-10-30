b135d1a15aa9f1f730640291a2d501e33eb2b5a6
Marcus Daly <marcusdaly@Marcuss-MacBook-Pro.local>
2014-10-29 22:35:19 -0400

    Initial Commit

---
+++ Unit Circle Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Unit Circle Quiz
+//
+//  Created by Marcus Daly on 10/29/14.
+//  Copyright (c) 2014 Marcus Daly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
